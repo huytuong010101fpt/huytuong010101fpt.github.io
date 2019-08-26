c1b753f035a1bd263e0388e5c7d209697cd590f9
Huy Tưởng 0965223769 <huytuong010101@gmail.com>
2019-08-26 23:57:34 +0700

    a

--- ManageCoffee/js/jsNV.js
+++ ManageCoffee/js/jsNV.js
@@ -1,7 +1,8 @@
 
 var Table = document.getElementsByClassName('tableCF');
 for (i = 0; i < Table.length;i++){
-	Table[i].onclick = function(e){
+	Table[i].onclick = function(event){
+		document.getElementsByClassName('TBNum')[0].innerText = "Bàn số " + event.target.id + ":";
 		document.getElementsByClassName('menu')[0].style.display = "block";
 	}
 }
